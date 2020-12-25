45504399ea6fade40e441323218cd6ab498e4b19
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-12-25 13:13:48 -0500

    connect t0 mongo db

--- config/db.js
+++ config/db.js
@@ -4,7 +4,10 @@ const config = require('config');
 const db = config.get('mongoURI');
 const connectDB = async () => {
     try {
-        await mongoose.connect(db);
+        await mongoose.connect(db, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+         });
         console.log('mongo db connected')
     } catch (err) {
         console.error(err.message);
