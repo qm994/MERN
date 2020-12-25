65ebc40d1adf2e15a805161b502c55bb8cf51a4b
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-12-25 12:58:20 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('This the test root url!')
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
