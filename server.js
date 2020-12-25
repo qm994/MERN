9a0839c3538e036e0b6b2cb9c17fc09d415b3b39
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-12-25 13:12:22 -0500

    connect t0 mongo db

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
+// connect to db 
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('This the test root url!')
 })
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
