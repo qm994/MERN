f544a538d75c47a017d790966c6c647f7d052075
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-12-25 19:56:46 -0500

    ap/auth route for get a new token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const config = require('config');
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 const User = require("../../models/User");
 // @route   GET api/auth
-// @desc    Test route
-// @access  Public
+// @desc    Get the user info by providing a token
+// @access  Private
 router.get('/', auth, async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password');
@@ -13,6 +17,72 @@ router.get('/', auth, async (req, res) => {
         console.error(e);
         res.status(500).send('Server error!')
     }
+});
+
+// @route   POST api/auth
+// @desc    Authenticated user & get a new token
+// @body    {email, password}
+// @access  Public
+router.post(
+    '/',
+    [
+        check('email', 'Please include a valid email!').isEmail(),
+        check(
+            'password',
+            'Password is required'
+        ).exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+           return res.status(400).json({
+               errors: errors.array()
+           })
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            // See if user email match: if user not registered yet, send error
+            let user = await User.findOne({ email: email });
+            if(!user) {
+                return res
+                    .status(400)
+                    .json({
+                        errors: [{ msg: 'Invalid credentials!' }]
+                    })
+            };
+            // See if user password match
+            const isMatch = await bcrypt.compare(password, user.password);
+            if(!isMatch) {
+                return res
+                    .status(400)
+                    .json({
+                        errors: [{ msg: 'Invalid credentials!' }]
+                    })
+            }; 
+            // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+            jwt.sign(
+                payload,
+                config.get("jwtsecret"),
+                { expiresIn: 3600000 },
+                (error, token) => {
+                    if(error) {
+                        throw error;
+                    }
+                    res.json({ token })
+                }
+            );
+        } catch (e) {
+            console.error(e);
+            // server error
+            res.status(500).send('Server Error')
+        }
 });
 
 module.exports = router;
