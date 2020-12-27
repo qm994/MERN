8c7c0a816c610164ed990564c3c56cc475183263
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-12-27 14:10:21 -0500

    ADD and DELETE comment ROUTE

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -181,5 +181,116 @@ router.put(
     }
 );
 
+// @route   POST api/posts/comment/:id
+// @desc    Create comment inside a post
+// @access  Private
+router.post(
+    '/comment/:id',
+    [
+        auth,
+        [
+            check('text', 'text inside comment is required!').not().isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                msg: errors.array()
+            })
+        };
+        try {
+            const user = await User.findById(req.user.id).select("-password");
+            const post = await Post.findById(req.params.id);
+            // Check if post existed
+            if(!post) {
+                return res.status(404).json({
+                    msg: "No such post!"
+                })
+            };
+
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            };
+            post.comments.unshift(newComment);
+            await post.save();
+            return res.status(200).json(post.comments)
+
+        } catch (e) {
+            console.error(e);
+            if(e.kind = "ObjectId") {
+                return res.status(404).json({
+                    msg: "No such post!"
+                })
+            };
+            res.sendStatus(500);
+        }
+});
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete the comment insdie the post by ID
+// @access  Private
+router.delete(
+    '/comment/:id/:comment_id',
+    auth,
+    async (req, res) => {
+        try {
+            const user = await User.findById(req.user.id).select("-password");
+            const post = await Post.findById(req.params.id);
+            // Check if post existed
+            if(!post) {
+                return res.status(404).json({
+                    msg: "No such post!"
+                })
+            };
+            const isRemovedCommentExist = post
+                .comments
+                .filter(
+                    comment => comment.id === req.params.comment_id
+                ).length > 0;
+                
+            // Check if comment existed
+            if(post.comments.length === 0 || !isRemovedCommentExist) {
+                return res.status(404).json({
+                    msg: "No such comment!"
+                })
+            };
+
+            // Check if user own this comment
+            const isUserOwnComment = post
+                .comments
+                .filter(
+                    comment => comment.user.toString() === req.user.id
+                ).length > 0;
+            
+            if(!isUserOwnComment) {
+                return res.status(403).json({
+                    msg: "User doesnt own this comment!"
+                })
+            };
+
+            const removedCommnetIdx = post
+                .comments
+                .map(comment => comment.id)
+                .indexOf(req.params.comment_id);
+    
+            post.comments.splice(removedCommnetIdx, 1);
+            await post.save();
+            return res.status(200).json(post.comments)
+
+        } catch (e) {
+            console.error(e);
+            if(e.kind = "ObjectId") {
+                return res.status(404).json({
+                    msg: "No such post/comment!"
+                })
+            };
+            res.sendStatus(500);
+        }
+});
+
 
 module.exports = router;
