e53d98ff717143a1dc2cec2db1ffae879da2a8d0
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2020-12-31 16:02:09 -0500

    add education action

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Dashboard from '../src/components/dashboard/Dashboard';
 import CreateProfile from '../src/components/profile-forms/CreateProfile';
 import EditProfile from '../src/components/profile-forms/EditProfile';
 import AddExperience from '../src/components/profile-forms/AddExperience';
+import AddEducation from '../src/components/profile-forms/AddEducation';
 
 import PrivateRoute from './components/routing/PrivateRoute';
 import setAuthToken from './utils/setAuthToken';
@@ -44,6 +45,7 @@ const App = ({ user }) => {
             <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
             <PrivateRoute exact path='/add-experience' component={AddExperience} />
+            <PrivateRoute exact path='/add-education' component={AddEducation} />
           </Switch>
         </section>
       </Fragment>
