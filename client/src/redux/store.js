5f237371b1f3de871b9b6b0660659dbbe4ff5300
Qingyuan Ma <qingyuan@Justins-MacBook-Pro-166.local>
2021-01-02 22:36:07 -0500

    add prod setup + 1

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { logger } from 'redux-logger';
+//import { logger } from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger, thunk];
+const middlewares = [thunk];
 const intialState = {};
 
 export const store = createStore(
